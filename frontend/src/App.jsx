a4e0c5443720c144d20aa2d26e30f8233e4b9af7
KarimElbasiouni <elbasiounikarim@gmail.com>
2025-02-02 11:23:30 -0500

    Add favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -171,18 +171,21 @@ function App() {
             <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
 
             <form className="form" onSubmit={handleSubmit}>
-              <div className="prompt-box">
-                <input
-                  className="prompt"
-                  placeholder="Enter location here"
-                  type="text"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-                <button type="submit" className="submitButton">
-                <svg className="submit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#f8fff5dd" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
-                </button>
-
+              <div className="input-group">
+                <label htmlFor="location" className="location-label">Location:</label>
+                <div className="prompt-box">
+                  <input
+                    id="location"
+                    className="prompt"
+                    placeholder="Enter location here"
+                    type="text"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                  />
+                  <button type="submit" className="submitButton">
+                    Submit
+                  </button>
+                </div>
               </div>
             </form>
           </div>
